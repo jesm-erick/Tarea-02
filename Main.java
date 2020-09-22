090803519cf112e21349ba23ba7c9ae47868b23d
jesm-erick <64477827+jesm-erick@users.noreply.github.com>
2020-09-21 22:55:04 -0500

    scanner

--- Main.java
+++ Main.java
@@ -1,28 +1,15 @@
 import entities.Person;
+
+import java.util.Scanner;
+
 import coche.Coche;
 
 public class Main {
 
     public static void main(String[] args) {
-        Person p = new Person();
-        p.setName("David "+p);
-        p.setSex("M");
-        System.out.println("sexo= "+ p.getSex() );
-        p.laugh();
-
-        Person p2 = new Person("Gloria","F");
-        System.out.println("sexo= "+ p2.getSex() );
-        p2.laugh();
-
-        Coche c = new Coche();
-        c.setMarca("Toyata "+c);
-        c.setColor("Rojo ");
-        System.out.println("Color= "+ c.getColor() );
-        c.Sound();
-
-        Coche c2 = new Coche("Alfa Romeo","Azul");
-        System.out.println("Color= "+ c2.getColor() );
-        c2.Sound();
+        Scanner input = new Scanner(System.in);
+        String name= input.nextLine();
+        System.out.println("escribiste: "+name);
 
 
 
