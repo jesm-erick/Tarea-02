a283318599fde6c9bd5d714ea170add24eb43afa
jesm-erick <64477827+jesm-erick@users.noreply.github.com>
2020-09-21 23:19:56 -0500

    Menu

--- Main.java
+++ Main.java
@@ -8,8 +8,29 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String name= input.nextLine();
-        System.out.println("escribiste: "+name);
+        //System.out.print("Ingrese nombre:");
+        //String name= input.nextLine();
+        //System.out.println("escribiste: "+name);
+        int opt = 0;
+        do{
+            System.out.println("***** CRUD PERSON *****");
+            System.out.println("1 List ");
+            System.out.println("2 New ");
+            System.out.println("3 Delete ");
+            System.out.println("0 Exit ");
+            System.out.println("Choice option: ");
+            opt = input.nextInt();
+            System.out.println("You chosed: " + opt);
+            input.nextLine(); // Limpiar el buffer
+            switch (opt) {
+                case 1: System.out.println("Opcion 1 elejida");
+                break;
+                case 2: System.out.println("Opcion 2 elejida");
+                break;
+                default: System.out.println("Opcion no valida");
+            
+            }
+        }while (opt != 0);
 
 
 
